239fe7d562964c1ad0c09ce260acb8a1ce200228
Amanur Rahman <aramancse@gmail.com>
2020-08-18 18:34:29 +0600

    successfully done

---
+++ app.js
@@ -0,0 +1,72 @@
+const searchSong= document.getElementById("searchSong");
+const lyricsShow = document.getElementById("lyricsShow");
+const apiUrl = `https://api.lyrics.ovh`;
+
+document.getElementById("searchBtn").addEventListener("click",e=>{
+    e.preventDefault();
+    const searchValue = searchSong.value.trim();
+    if(!searchValue){
+        alert("Nothing Searched");
+    }
+    else{
+        getSong(searchValue);
+    }
+})
+
+async function getSong(searchValue) {
+    const res = await fetch(`${apiUrl}/suggest/${searchValue}`)
+    const data = await res.json();
+
+    showData(data);
+
+}
+
+function showData(data) {
+    lyricsShow.innerHTML = `
+    <ul class="song-list">
+    ${data.data.slice(0,10).map(song => `<li>
+                        <div class="single-result row align-items-center my-3 p-3">
+                    <div class="col-md-9">
+                        <img src="${song.album.cover}">
+                        <h3 class="lyrics-name">${song.title}</h3>
+                        <p class="author lead">Album by <span>${song.artist.name}</span></p>
+                    </div>
+                    <div class="col-md-3 text-md-right text-center">
+                        <button class="btn btn-success"> <span data-artist="${song.artist.name}" data-songTitle="${song.title}"> Get Lyrics </span> </button>
+                    </div>
+                </div>
+                </li>
+    `).join('')
+}
+    `
+}
+
+lyricsShow.addEventListener("click", e=>{
+    const clickedElement = e.target;
+    if (clickedElement.tagName === "span" || "button") {
+        const artist = clickedElement.getAttribute('data-artist');
+        const songTitle = clickedElement.getAttribute('data-songTitle');
+
+        getLyrics(artist, songTitle);
+    }
+})
+
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${apiUrl}/v1/${artist}/${songTitle}`);
+    const data = await res.json(); 
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    lyricsShow.innerHTML =    ` <div class="d-flex justify-content-end">
+                            <button class="btn btn-success" id="reload" onclick="reload()"> Search New Song Lyrics </button>
+                            </div>
+                            <br>
+                            <div class="single-lyrics text-center">
+                                <h2 class="text-success mb-4"><strong>${songTitle}</strong><br>- ${artist}</h2>
+                                 <pre class="lyric text-white" id="textCopy"><span  id="textCopy">${lyrics}</span></pre>
+                             </div>
+                             `
+}
+
+            function reload() {
+            window.location.reload(true);
+        }
